912f8c580aa6a651daa9b6270eb0d2a0e2f94af9
viro <twikista@gmail.com>
2022-09-22 20:00:43 +0100

    refactored resume component to render start and end dates

--- src/components/Resume/EducationItem.js
+++ src/components/Resume/EducationItem.js
@@ -2,24 +2,29 @@ import React, { Component } from "react";
 
 class EducationItem extends Component {
   render() {
-    const { duration, qualification, school } = this.props.data;
+    const {
+      startMonth,
+      startYear,
+      endMonth,
+      endYear,
+      qualification,
+      school,
+      city,
+    } = this.props.data;
     return (
       <tr>
-        <td className={"label"}>{duration}</td>
         <td
-          className={"info"}
-          // style={{
-          //   width: "60%",
-          //   verticalAlign: "top",
-          //   paddingBottom: "5px",
-          //   textAlign: "right",
-          // }}
-        >
+          className={"label"}
+        >{`${startMonth} ${startYear} - ${endMonth} ${endYear}`}</td>
+        <td className={"info"}>
           <span className="qualification" style={{ fontWeight: "600" }}>
             {qualification}
           </span>
           <span className="school" style={{ display: "block" }}>
             {school}
+          </span>
+          <span className="school" style={{ display: "block" }}>
+            {city}
           </span>
         </td>
       </tr>
--- src/components/Resume/EmploymentItem.js
+++ src/components/Resume/EmploymentItem.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 
 class EmploymentItem extends Component {
   render() {
-    const { company, period, role } = this.props.data;
+    const { company, role, startMonth, startYear, endMonth, endYear, city } =
+      this.props.data;
     return (
       <tr>
-        <td className="label">{period}</td>
+        <td className="label">{`${startMonth} ${startYear} - ${endMonth} ${endYear}`}</td>
         <td
           className="info"
           style={{ width: "60%", verticalAlign: "top", paddingBottom: "5px" }}
@@ -15,6 +16,9 @@ class EmploymentItem extends Component {
           </span>
           <span className="company" style={{ display: "block" }}>
             {company}
+          </span>
+          <span className="company" style={{ display: "block" }}>
+            {city}
           </span>
         </td>
       </tr>
